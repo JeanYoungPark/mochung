99d9a0c6f90b07505a71e14a2ee8580aea652f8d
JeanYoungPark <jean.young.park.96@gmail.com>
2025-02-17 16:27:11 +0900

    chore: update folder structure

--- src/app/editor/mCard/layout.tsx
+++ src/app/editor/mCard/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-export const layout = () => {
+export default function layout() {
     return <div>layout</div>;
-};
+}
---
+++ src/app/list/mobileInvitation/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MobileInvitationPage() {
+    return <div>MobileInvitationPage page</div>;
+}
