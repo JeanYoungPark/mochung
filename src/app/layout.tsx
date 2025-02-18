1f28346ecb732278e7da0c48bcb9084f79e89222
Jean Young Park <jean.young.park.96@gmail.com>
2025-02-18 21:56:41 +0900

    style: update layout style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang='en'>
-            <body className={`${pretendard.variable} ${poppins.variable} antialiased`}>
+            <body className={`${pretendard.variable} ${poppins.variable} antialiased mx-auto max-w-7xl w-full`}>
                 <header>
                     <Header />
                 </header>
